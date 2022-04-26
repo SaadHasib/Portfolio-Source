9816cc9595a46b129f0beae701578d2a6d6d01c3
Saad Hasib <saadhasib@outlook.com>
2022-04-27 02:35:16 +0500

    Added google fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,11 @@
 module.exports = {
-  content: [
-      "./src/**/*.{js,jsx,ts,tsx}",
-],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+    content: ["./src/**/*.{js,jsx,ts,tsx}"],
+    theme: {
+        fontFamily: {
+            body: ["Montserrat", "sans-serif"],
+            heading: ["Lato", "sans-serif"],
+        },
+        extend: {},
+    },
+    plugins: [],
+};
