8c1a67e91d1e0e4edc0e8b0511dfe1d01f2908aa
Saad Hasib <saadhasib@outlook.com>
2022-04-27 02:47:52 +0500

    Main layout hover effects

--- src/components/main_layout.tsx
+++ src/components/main_layout.tsx
@@ -9,22 +9,34 @@ function MainLayout(props: any) {
             border-b-white font-body font-bold"
             >
                 <Link className="inline-block ml-9 mt-2" to="/">
-                    <div className="w-36 h-2/3 border-b-4 border-b-green-500">
+                    <div
+                        className="w-[150px] h-[31px] border-b-4 border-b-green-500 \
+                    hover:h-[38px] hover:text-2xl hover:text-green-500 transition-all duration-200"
+                    >
                         Home
                     </div>
                 </Link>
                 <Link className="inline-block ml-auto mt-2" to="/blog">
-                    <div className="w-36 h-2/3 border-b-4 border-b-blue-500">
+                    <div
+                        className="w-[150px] h-[31px] border-b-4 border-b-blue-500 \
+                    hover:h-[38px] hover:text-2xl hover:text-blue-500 transition-all duration-200"
+                    >
                         Blog
                     </div>
                 </Link>
                 <Link className="inline-block mt-2" to="/experiments">
-                    <div className="w-36 h-2/3 border-b-4 border-b-blue-500">
+                    <div
+                        className="w-[150px] h-[31px] border-b-4 border-b-blue-500 \
+                    hover:h-[38px] hover:text-2xl hover:text-blue-500 transition-all duration-200"
+                    >
                         Experiments
                     </div>
                 </Link>
                 <Link className="inline-block mr-9 mt-2" to="/portfolio">
-                    <div className="w-36 h-2/3 border-b-4 border-b-blue-500">
+                    <div
+                        className="w-[150px] h-[31px] border-b-4 border-b-blue-500 \
+                    hover:h-[38px] hover:text-2xl hover:text-blue-500 transition-all duration-200"
+                    >
                         Portfolio
                     </div>
                 </Link>
