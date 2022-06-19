374dd2d563f6456f798ffb4496f00040633484ed
Saad Hasib <saadhasib@outlook.com>
2022-06-19 19:46:45 +0500

    Separated blog card component

---
+++ src/components/blog_card.tsx
@@ -0,0 +1,83 @@
+import * as React from "react";
+import { Link } from "gatsby";
+import { StaticImage } from "gatsby-plugin-image";
+
+type postCardData = {
+    frontmatter: {
+        title: string;
+        slug: string;
+        excerpt: string;
+        tags: string;
+    };
+    id: string;
+};
+
+type cardProps = {
+    node: postCardData;
+};
+
+function BlogCard(props: cardProps) {
+    return (
+        <div
+            className="bg-black
+            shadow-zinc-900 md:shadow-zinc-900
+            flex flex-col md:flex-row
+            shadow-md md:shadow-lg
+            w-11/12 lg:w-8/12
+            min-h-[300px] lg:min-h-[400px]
+            "
+        >
+            {/* Image */}
+            <StaticImage
+                src="../../../static/imgs/post_thumbs/post_thumb1.png"
+                alt="post thumbnail"
+                className="shrink-0 
+                w-auto h-[100px] object-cover
+                md:w-[300px] md:h-auto
+                lg:w-[400px] lg:h-auto"
+            ></StaticImage>
+            {/* Text container */}
+            <div
+                className="flex flex-col
+                items-centre grow
+                w-full"
+            >
+                <Link
+                    key={props.node.id}
+                    to={`/blog/${props.node.frontmatter.slug}`}
+                    className="block font-bold
+                border-b-2 border-white
+                w-full
+                text-lg md:text-xl
+                px-2 md:px-3
+                pt-3 md:pt-5
+                pb-2 md:pb-3
+                transition-all duration-200
+                md:hover:text-blue-500"
+                >
+                    {props.node.frontmatter.title}
+                </Link>
+                <div
+                    className="font-normal
+                text-md md:text-lg
+                px-2 md:px-3
+                pt-3 md:pt-5
+                pb-2 md:pb-3
+                "
+                >
+                    {props.node.frontmatter.excerpt}
+                </div>
+                <div
+                    className="font-normal text-zinc-400
+                text-sm md:text-base
+                px-5 pt-2 pb-4
+                mt-auto"
+                >
+                    {props.node.frontmatter.tags}
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default BlogCard;
--- src/pages/blog/index.tsx
+++ src/pages/blog/index.tsx
@@ -1,9 +1,9 @@
 import * as React from "react";
-import { graphql, Link } from "gatsby";
-import { StaticImage } from "gatsby-plugin-image";
+import { graphql } from "gatsby";
 import { Helmet } from "react-helmet";
 import MainLayout from "../../components/main_layout";
 import HeaderSection from "../../components/header";
+import BlogCard from "../../components/blog_card";
 
 type blogIdxProps = {
     data: {
@@ -54,65 +54,7 @@ function BlogHome(props: blogIdxProps) {
                 {/* Blog card */}
                 {props.data.allMarkdownRemark.nodes.map(
                     (node: postCardData): JSX.Element => (
-                        <div
-                            className="bg-black
-                            shadow-zinc-900 md:shadow-zinc-900
-                            flex flex-col md:flex-row
-                            shadow-md md:shadow-lg
-                            w-11/12 lg:w-8/12
-                            min-h-[300px] lg:min-h-[400px]
-                            "
-                        >
-                            {/* Image */}
-                            <StaticImage
-                                src="../../../static/post_thumb1.png"
-                                alt="post thumbnail"
-                                className="shrink-0 
-                                w-auto h-[100px] object-cover
-                                md:w-[300px] md:h-auto
-                                lg:w-[400px] lg:h-auto"
-                            ></StaticImage>
-                            {/* Text container */}
-                            <div
-                                className="flex flex-col
-                                items-centre grow
-                                w-full"
-                            >
-                                <Link
-                                    key={node.id}
-                                    to={`/blog/${node.frontmatter.slug}`}
-                                    className="block font-bold
-                                border-b-2 border-white
-                                w-full
-                                text-lg md:text-xl
-                                px-2 md:px-3
-                                pt-3 md:pt-5
-                                pb-2 md:pb-3
-                                transition-all duration-200
-                                md:hover:text-blue-500"
-                                >
-                                    {node.frontmatter.title}
-                                </Link>
-                                <div
-                                    className="font-normal
-                                text-md md:text-lg
-                                px-2 md:px-3
-                                pt-3 md:pt-5
-                                pb-2 md:pb-3
-                                "
-                                >
-                                    {node.frontmatter.excerpt}
-                                </div>
-                                <div
-                                    className="font-normal text-zinc-400
-                                text-sm md:text-base
-                                px-5 pt-2 pb-4
-                                mt-auto"
-                                >
-                                    {node.frontmatter.tags}
-                                </div>
-                            </div>
-                        </div>
+                        <BlogCard node={node} />
                     )
                 )}
             </div>
